7988043876c366223f5d3f0a5da34c8287a59865
Peter Keen <peter.keen@bugsplat.info>
2012-04-12 15:03:53 -0700

    Initial Commit

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,10 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',            # required
+    :aws_access_key_id      => ENV['S3_ACCESS'], # required
+    :aws_secret_access_key  => ENV['S3_SECRET'], # required
+  }
+  config.fog_directory  = 'bugsplat-images'                     # required
+  config.fog_public     = false                                   # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phytos::Application.config.secret_token = '1d1c891e3a0ea500abe345e8abc58c96504ff8415aa2c2bbe717952216bef3e6844c16d9ace47e9ce24cd443edbb766699c6b3116d7bb518b6cb762a9c94e5af'
