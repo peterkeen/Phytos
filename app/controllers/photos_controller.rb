c6f4c469baa4b5e1ded7dc685ae99dc5b2f0df34
Peter Keen <peter.keen@bugsplat.info>
2012-04-12 19:44:47 -0700

    Make it pretty

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,7 +5,7 @@ class PhotosController < ApplicationController
   # GET /photos
   # GET /photos.json
   def index
-    @photos = Photo.all
+    @photos = Photo.order('name desc').all
 
     respond_to do |format|
       format.html # index.html.erb
